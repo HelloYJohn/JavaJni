7cf33d9ee48bc4654ed0a1d7b50e82c8499ec400
yuanwenxing <yuanwenxing@yuanwenxing>
2019-06-17 10:35:13 +0800

    JavaExample2 update

--- JavaExample2/src/com/esgyn/MyTest.java
+++ JavaExample2/src/com/esgyn/MyTest.java
@@ -13,20 +13,39 @@ public class MyTest {
       System.out.println(magic_counter);
   }
   
-  public static void testBytes(byte[] bytes, int length) throws UnsupportedEncodingException {
-	  System.out.println(new String(bytes, "UTF-8"));
-	  long heapSize = Runtime.getRuntime().totalMemory();
-	  System.out.println(heapSize);
-	  char[] c1 = new char[(int) (Runtime.getRuntime().maxMemory()*0.29)];
-	  //获取堆的最大大小byte单位
-	  //超过将抛出 OutOfMemoryException
-	  // char[] c2 = new char[(int) (Runtime.getRuntime().maxMemory()*0.29)];
-	  long heapMaxSize = Runtime.getRuntime().maxMemory();
-	  System.out.println(heapMaxSize);
-		
-	  //获取当前空闲的内存容量byte单位
-	  long heapFreeSize = Runtime.getRuntime().freeMemory();
-	  System.out.println(heapFreeSize);
+  public static void testBytes(byte[] bytes, int length) {
+	  try {
+		  System.out.println(new String(bytes, "UTF-8").length());
+		  long heapSize = Runtime.getRuntime().totalMemory();
+		  System.out.println(heapSize);
+		  //获取堆的最大大小byte单位
+		  //超过将抛出 OutOfMemoryException
+		  // char[] c2 = new char[(int) (Runtime.getRuntime().maxMemory()*0.29)];
+		  long heapMaxSize = Runtime.getRuntime().maxMemory();
+		  System.out.println(heapMaxSize);
+			
+		  //获取当前空闲的内存容量byte单位
+		  long heapFreeSize = Runtime.getRuntime().freeMemory();
+		  System.out.println(heapFreeSize);
+	  } catch (UnsupportedEncodingException e) {
+		  // TODO: handle exception
+		  e.printStackTrace();
+	  } catch (Exception e) {
+		// TODO: handle exception
+		  e.printStackTrace();
+	  }catch (OutOfMemoryError e) {
+		  // TODO: handle exception
+		  // flag[0] = true;
+		  // e.printStackTrace();
+		  try {
+			throw new Exception("Out of memory");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	  } catch (Throwable e) {
+		  e.printStackTrace();
+	  }
   }
   
   public static void main(String[] args) throws UnsupportedEncodingException {
